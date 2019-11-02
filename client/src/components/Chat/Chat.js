1de7c31593b2f22f4741948c8a41e3e5d9c9c2cd
Ruslan Kurbangaliev <admin.r@icloud.com>
2019-11-03 02:15:35 +0300

    add text container

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -7,12 +7,14 @@ import './Chat.css';
 import InfoBar from '../InfoBar/InfoBar';
 import Input from '../Input/Input';
 import Messages from '../Messages/Messages';
+import TextContainer from '../TextContainer/TextContainer';
 
 let socket;
 
 const Chat = ({ location }) => {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
+    const [users, setUsers] = useState('');
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
     const ENDPOINT = 'localhost:5000';
@@ -25,21 +27,34 @@ const Chat = ({ location }) => {
         setRoom(room);
         setName(name);
 
-        socket.emit('join', { name, room }, () => {
-
+        socket.emit('join', { name, room }, (error) => {
+            if(error) {
+                alert(error);
+            }
         });
         
-        return () => {
+        /*return () => {
             socket.emit('disconnect');
 
             socket.off();
-        }
+        }*/
     }, [ENDPOINT, location.search])
 
     useEffect(() => {
         socket.on('message', (message) => {
             setMessages([...messages, message]);
         })
+
+        socket.on('roomData', ({ users }) => {
+            setUsers(users);
+        })
+
+        return () => {
+            socket.emit('disconnect');
+      
+            socket.off();
+        }
+
     }, [messages])
 
     const sendMessage = (event) => {
@@ -58,6 +73,7 @@ const Chat = ({ location }) => {
                 <Messages messages={messages} name={name} />
                 <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
             </div>
+            <TextContainer users={users}/>
         </div>
     )
 }
